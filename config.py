b4ed42ec5a0eb596a05d4e7d36db8400554f2cfb
Rohit-hub616 <ruhilrohit9@gmail.com>
2025-04-27 12:23:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7890781002,6373737837]
+api_id = "25967358"
+api_hash = "10a5a31171dfdc323efdbcf84a8fb016"
+bot_token = "7059710796:AAHustbji45EixwjP0GEmiN2qn4tsAngQag"
+auth_users = [7733608738]
